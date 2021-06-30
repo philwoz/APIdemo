941147c0084dcd98cd6aecf997aaf59a67cf7f83
philwoz <61164019+philwoz@users.noreply.github.com>
2021-06-30 11:11:22 +0100

    axios fetching api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import axios from "axios";
+
+
+require("dotenv").config();
 
 function App() {
+  const [data, setData] = React.useState(null);
+  // const [leagues, setLeagues] = React.useState(4);
+
+  React.useEffect(() => {
+    const getFixtures = () => {
+      const config = {
+        method: 'get',
+        url: 'https://v3.football.api-sports.io/status',
+        headers: {
+          'x-rapidapi-key': process.env.API_KEY,
+          'x-rapidapi-host': 'v3.football.api-sports.io'
+        }
+      };
+
+      axios(config)
+        .then(function (response) {
+          let a = JSON.stringify(response.data.response);
+          // console.log(JSON.stringify(response.data));
+          // console.log(response.data.response)
+          setData(a)
+          console.log(data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      
+    }
+    getFixtures();
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>{data ? [data].map((item) => {
+      return <h1>{item}</h1>
+    }) : <h1>null</h1>}</div>
+  )
+
+
 }
+
 
 export default App;
